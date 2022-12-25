462a0492c5caa5c10e0d0f70d166f2016a024726
vidok0577 <vidok0577@gmail.com>
2022-12-25 19:47:40 +0500

    add task 3, ya_disk

--- ya_disk.py
+++ ya_disk.py
@@ -1,16 +1,26 @@
+import requests
+import os.path as path
+import config
+
 class YaUploader:
     def __init__(self, token: str):
-        self.token = token
+        self.HOST = 'https://cloud-api.yandex.net:443'
+        self.headers = {'Accept': 'application/json', 'Authorization': token}
+        self.upload_uri = '/v1/disk/resources/upload'
 
     def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
-
+        url = self.HOST + self.upload_uri
+        file_ = path.basename(file_path)
+        params = {'path': file_, 'overwrite': 'true'}
+        response = requests.get(url, headers=self.headers, params=params)
+        upload_resp = requests.put(response.json()['href'], headers=self.headers, data=open(file_, 'rb'))
+        if upload_resp.status_code == 200 or 201:
+            print('Всё получилось.')
+        else:
+            print('Что-то пошло не так.')
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
+    path_to_file = 'test.txt' #input('Введите путь к файлу: ')
+    token = config.TOKEN
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
